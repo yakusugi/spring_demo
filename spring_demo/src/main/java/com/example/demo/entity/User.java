8883703aa64a7f4f7561ba92788f2551a59209e9
yosuke <yosuke@yosuke-Galago-Pro>
2021-12-17 23:02:42 +0900

    first commit and push

---
+++ spring_demo/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.demo.entity;
+import java.util.Date;
+import lombok.Data;
+/**
+ * ユーザー情報 Entity
+ */
+@Data
+public class User {
+    /**
+     * ID
+     */
+    private Long id;
+    /**
+     * 名前
+     */
+    private String name;
+    /**
+     * 住所
+     */
+    private String address;
+    /**
+     * 電話番号
+     */
+    private String phone;
+    /**
+     * 更新日時
+     */
+    private Date updateDate;
+    /**
+     * 登録日時
+     */
+    private Date createDate;
+    /**
+     * 削除日時
+     */
+    private Date deleteDate;
+}
